f77bb259bd2bef1bade5903d66c0686b3c070a25
Andrew Quitadamo <andrew.quitadamo@gmail.com>
2016-02-15 17:21:00 -0500

    Added code to filter snps

---
+++ code/filtersnps.R
@@ -0,0 +1,29 @@
+args = commandArgs(trailingOnly=TRUE)
+
+if (length(args)==0)
+{
+  stop("Input file must be supplied.", call.=FALSE)
+}
+
+input_file = args[1]
+
+SNPMatrix <- read.table(input_file,header=T,row.names=1,check.names=F,na.string="NA")
+
+filter_NA <- function(snps, percent)
+{
+	stopifnot(percent < 1 || percent > 0)
+	if (sum(is.na(snps))/length(snps) <= 1-percent)
+	{
+		return(TRUE)
+	}
+	else
+	{
+		return(FALSE)
+	}
+}
+
+filtered_snps <- apply(SNPMatrix, 1, filter_NA, 0.9)
+FilteredSNPMatrix<- SNPMatrix[filtered_snps,]
+FilteredSNPMatrix <- cbind(rownames(FilteredSNPMatrix), FilteredSNPMatrix)
+colnames(FilteredSNPMatrix)[1] = "ID"
+write.table(FilteredSNPMatrix,paste(input_file,'.filtered',sep=""), quote=F,sep="\t",row.names=F,col.names=T)
